e248f5135b4ab7935520001292d268e900164bcb
Joakim Hove <joakim.hove@gmail.com>
2019-01-14 13:42:43 +0100

    Fix const correctness

--- lib/analysis/modules/ies_enkf_data.h
+++ lib/analysis/modules/ies_enkf_data.h
@@ -18,7 +18,7 @@ void ies_enkf_data_set_iteration_nr( ies_enkf_data_type * data , int iteration_n
 int ies_enkf_data_get_iteration_nr( const ies_enkf_data_type * data );
 int ies_enkf_data_inc_iteration_nr( ies_enkf_data_type * data);
 
-const ies_enkf_config_type* ies_enkf_data_get_config(const ies_enkf_data_type * data);
+ies_enkf_config_type* ies_enkf_data_get_config(const ies_enkf_data_type * data);
 
 void ies_enkf_data_update_ens_mask(ies_enkf_data_type * data, const bool_vector_type * ens_mask);
 void ies_enkf_store_initial_obs_mask(ies_enkf_data_type * data, const bool_vector_type * obs_mask);
@@ -40,8 +40,8 @@ void ies_enkf_data_allocateW(ies_enkf_data_type * data, int ens_size);
 void ies_enkf_data_store_initialE(ies_enkf_data_type * data, const matrix_type * E0);
 void ies_enkf_data_store_initialA(ies_enkf_data_type * data, const matrix_type * A);
 const matrix_type * ies_enkf_data_getE(const ies_enkf_data_type * data);
-const matrix_type * ies_enkf_data_getW(const ies_enkf_data_type * data);
 const matrix_type * ies_enkf_data_getA0(const ies_enkf_data_type * data);
+matrix_type * ies_enkf_data_getW(const ies_enkf_data_type * data);
 
 bool ies_enkf_data_get_AAprojection(const ies_enkf_data_type * data);
 
